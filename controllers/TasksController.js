118215c45675c25a072247f6fb14badd871e03c2
3str <ssrghome@gmail.com>
2022-12-13 11:15:17 +0300

    password check

--- controllers/TasksController.js
+++ controllers/TasksController.js
@@ -13,7 +13,13 @@ class TaskController {
   }
 
   add(req, res) {
+    if (req.body.password !== process.env.POST_PASSWORD) {
+      res.json("Неверный пароль");
+      return;
+    }
+    delete req.body.password;
     const task = { id: v4(), taskId: tasks.at(-1).taskId + 1, ...req.body };
+    console.log(task);
     tasks.push(task);
     fs.writeFile(
       path.resolve("data", "tasks.json"),
