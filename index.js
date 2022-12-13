d85ef54d6053dfd6742bcf57a4ee4a9ba3474e53
3str <ssrghome@gmail.com>
2022-12-13 17:49:10 +0300

    update: CORS origin

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*"],
+  })
+);
 
 app.use("/api", ApiRouter);
 
